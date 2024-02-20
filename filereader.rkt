c57351a82786e68705d3b92fbbb6c2b39ab04297
Gérard Lichtert <gerard.lichtert@hotmail.com>
2024-02-20 13:01:55 +0100

    added filereaders and support in constants

--- filereader.rkt
+++ filereader.rkt
@@ -1,5 +1,7 @@
 #lang r5rs
 
+(#%provide make-filereader-adt)
+
 (define (make-filereader-adt filename)
   (let* ((port (open-input-file filename))
          (content (read port)))
@@ -10,15 +12,24 @@
     (define pacman-announcer 'pacman)
     (define ghosts-announcer 'ghosts)
 
-    (define get-coins-positions (assq coins-announcer content))
-    (define get-wall-positions (assq walls-announcer content))
-    (define get-pacman-position (assq pacman-announcer content))
-    (define get-ghost-range (assq ghosts-announcer content))
+    (define (make-get-function key)
+      (lambda ()
+        (let ((result (assq key content)))
+          (if result
+              (cadr result)
+              result))))
+
+    (define get-coins-positions ((make-get-function coins-announcer)))
+    (define get-wall-positions ((make-get-function walls-announcer)))
+    (define get-pacman-position ((make-get-function pacman-announcer)))
+    (define get-ghost-range ((make-get-function ghosts-announcer)))
 
 
     (define filereader-dispatch
       (lambda (message)
         (cond
-          ((eq? message 'get-coins-positions) get-coins-positions)
-          ((eq? message 'content) content))))
+          ((eq? message 'get-wall-positions) get-wall-positions)
+          ((eq? message 'get-pacman-position) get-pacman-position)
+          ((eq? message 'get-ghost-range) get-ghost-range)
+          ((eq? message 'get-coins-positions) get-coins-positions))))
     filereader-dispatch))
